d632b2a0aae5c1c9b0bd004b35667a108f3c7be2
Kaskar <driveseckota@gmail.com>
2024-12-23 17:56:44 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: 'VOTRE_TOKEN_ICI', // Remplacez par votre token
+    clientId: 'VOTRE_CLIENT_ID_ICI', // Remplacez par votre client ID
+    guildId: 'VOTRE_GUILD_ID_ICI' // Remplacez par l'ID de votre serveur (facultatif, mais nécessaire pour enregistrer les commandes sur un serveur spécifique)
+  };
+  
